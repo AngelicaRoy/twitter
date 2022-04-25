f4fae99d5fedf1e3c37c1d302adad952ef113abd
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-24 19:56:56 -0500

    move models in app folder

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,4 +1,4 @@
-const user=require('./../../app/models/user')
+const user=require('/../../app/models/user')
 
 descrive("Unit Test for User class", () => {
     test('Create an User object', () => {
