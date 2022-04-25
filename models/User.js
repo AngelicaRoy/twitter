6557d29cb5cb7cc3c27754f822b38365e1eae757
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-24 19:46:01 -0500

    Add constructor to User class

--- models/User.js
+++ models/User.js
@@ -1,19 +1,10 @@
-const User=require('./../../app/models/user')
-
-descrive("Unit Test for User class", () => {
-    test('Create an User object', () => {
-        //Aqui colocas el codigo que vas a usar en la app
-        const user=new User(1,"carlogilmar", "Carlo", "Bio",
-        "dataCreated","lastUpdated")
-
-        //Aqui validas los resultados de ese codigo
-        //Esta es una comporacion que va a igualar el valor de la izquierda
-        //con el valor de la derecha(valor esperado)
-        expect(user.id).toBe(1)
-        expect(user.username).toBe("carlogilmar")
-        expect(user.name).toBe("Carlo")
-        expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
-    });
-})
+class User{
+    constructor(id, username, name, dateCreated, lastUpdated){
+        this.id=id
+        this.username=username
+        this.name=name
+        this.bio=bio
+        this.dateCreated=dateCreated
+        this.lastUpdated=lastUpdated
+    }
+}
