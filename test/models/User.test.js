47a061495172f9f0586e3b589ce242870eef952e
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-24 20:14:19 -0500

    Update name and params in user const

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,10 +1,10 @@
-const user=require('./../app/models/user')
+const User=require('./../../app/models/user')
 
-descrive("Unit Test for User class", () => {
+describe("Unit Test for User class", () => {
     test('Create an User object', () => {
         //Aqui colocas el codigo que vas a usar en la app
         const user=new User(1,"carlogilmar", "Carlo", "Bio",
-        "dataCreated","lastUpdated")
+        "dateCreated","lastUpdated")
 
         //Aqui validas los resultados de ese codigo
         //Esta es una comporacion que va a igualar el valor de la izquierda
