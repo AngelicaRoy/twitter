3e223a4a93f329da04ae8e9d8ce42d8906514e60
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-24 20:18:36 -0500

    Change dateCreated and lastUpdated to undefined

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,8 +3,7 @@ const User=require('./../../app/models/user')
 describe("Unit Test for User class", () => {
     test('Create an User object', () => {
         //Aqui colocas el codigo que vas a usar en la app
-        const user=new User(1,"carlogilmar", "Carlo", "Bio",
-        "dateCreated","lastUpdated")
+        const user=new User(1,"carlogilmar", "Carlo", "Bio")
 
         //Aqui validas los resultados de ese codigo
         //Esta es una comporacion que va a igualar el valor de la izquierda
@@ -13,7 +12,7 @@ describe("Unit Test for User class", () => {
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()//verifica que el valor no sea undefie
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
